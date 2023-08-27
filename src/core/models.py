ac23bde2ddbb6840178574e865215468fb483ea2
FalseR20 <kno3.2002@gmail.com>
2023-08-27 15:26:20 +0300

    docs: add courses and currencies

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    code3 = models.CharField(max_length=3)
+
+
+class Cource(models.Model):
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    course = models.DecimalField(max_digits=16, decimal_places=4)
+    update_time = models.DateTimeField()
